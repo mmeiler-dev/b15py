198c7fe34bc21bd1d3f8e14426379d9c21f4095e
mmeiler-dev <maxmeiler@proton.me>
2025-03-27 23:19:45 +0100

    fix: added missing requirement matplotlib

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import find_packages, setup
 setup(
     name="b15py",
     packages=find_packages(include=["b15py"]),
-    version="1.0.3",
+    version="1.0.4",
     description="Ein Module um das B15F-Board mit Python zu programmieren.",
     author="mmeiler-dev",
     install_requires=[
         "pyserial>=3.5",
-        "typeguard>=4.4.1"
+        "typeguard>=4.4.1",
+        "matplotlib>=3.10.1"
     ]
 )
